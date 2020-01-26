9494eb2ac7c0ce870c9989df1c5bc9446ecab3e5
韩贵封 <hanguifeng@hanguifengdeMacBook-Pro-2.local>
2020-01-26 22:50:38 +0800

    189.rotate-array

---
+++ LeetCode/189.rotate-array/rotateArray.jsx
@@ -0,0 +1,34 @@
+let rotate = function(nums, k) {
+  const items = [];
+  for (i = 0; i < nums.length; i++) {
+    items[(i + k) % nums.length] = nums[i];
+  }
+  return items;
+};
+
+let rotate = function(nums, k) {
+  let n = nums.length;
+  // 原数组操作
+  nums.splice(0, 0, ...nums.splice(n - (k % n)));
+};
+
+let rotate = function(nums, k) {
+  const n = nums.length;
+  k %= n;
+  if (n == 1) {
+    return;
+  }
+  let tmp = 0;
+  myReverse(0, n - 1);
+  myReverse(0, k - 1);
+  myReverse(k, n - 1);
+  const myReverse = (start, end) => {
+    while (start < end) {
+      tmp = nums[start];
+      nums[start] = nums[end];
+      nums[end] = tmp;
+      start++;
+      end--;
+    }
+  };
+};
